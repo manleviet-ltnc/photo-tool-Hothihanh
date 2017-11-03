f0947b17672911bf2011d5e01e2d29801b85ec3b
Trần Phương Anh <31817291+trphuonganh@users.noreply.github.com>
2017-11-03 12:26:47 +0700

    Photo Tool 6
    epi 6

--- MyPhotos/MyPhotoControls/PhotoEditDialog.cs
+++ MyPhotos/MyPhotoControls/PhotoEditDialog.cs
@@ -50,7 +50,7 @@ namespace Manning.MyPhotoControls
         {
             _photo = photo;
             ResetDialog();
-            mskDateTaken.ValidatingType = typeof(CurrentDate);
+           
         }
 
         protected override void ResetDialog()
@@ -75,7 +75,7 @@ namespace Manning.MyPhotoControls
             {
                 txtPhotoFile.Text = photo.FileName;
                 txtCaption.Text = photo.Caption;
-                mskDateTaken.Text = photo.DateTaken.ToString();
+                dtpDateTaken.Value = photo.DateTaken;
                 cmbPhotographer.Text = photo.Photographer;
                 txtNotes.Text = photo.Notes;
             }
@@ -97,27 +97,16 @@ namespace Manning.MyPhotoControls
                 photo.Notes = txtNotes.Text;
                 try
                 {
-                    photo.DateTaken = DateTime.Parse(mskDateTaken.Text);
+                    photo.DateTaken = dtpDateTaken.Value;
                 }
                 catch (FormatException) { }
             }
+
         }
 
         private void txtCaption_TextChanged(object sender, EventArgs e)
         {
             Text = txtCaption.Text + " - Properties";
-        }
-
-        private static class CurrentDate
-        {
-            public static DateTime Parse(string input)
-            {
-                DateTime result = DateTime.Parse(input);
-                if (result > DateTime.Now)
-                    throw new FormatException("The given date is in the future.");
-
-                return result;
-            }
         }
 
         private void mskDateTaken_TypeValidationCompleted(object sender, TypeValidationEventArgs e)
