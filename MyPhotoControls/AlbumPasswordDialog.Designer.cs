57c8186026e0d993fe1c86dc866e63ca4920773b
Trần Phương Anh <31817291+trphuonganh@users.noreply.github.com>
2017-11-08 04:16:28 +0700

    photo tool 7
    photo tool 7

---
+++ MyPhotoControls/AlbumPasswordDialog.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Manning.MyPhotoControls
+{
+    partial class AlbumPasswordDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAlbum = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.llblOK = new System.Windows.Forms.LinkLabel();
+            this.llblCancel = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "&Album File:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "&Password:";
+            // 
+            // txtAlbum
+            // 
+            this.txtAlbum.Location = new System.Drawing.Point(76, 6);
+            this.txtAlbum.Name = "txtAlbum";
+            this.txtAlbum.ReadOnly = true;
+            this.txtAlbum.Size = new System.Drawing.Size(181, 20);
+            this.txtAlbum.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(76, 32);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(181, 20);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // llblOK
+            // 
+            this.llblOK.AutoSize = true;
+            this.llblOK.Location = new System.Drawing.Point(12, 75);
+            this.llblOK.Name = "llblOK";
+            this.llblOK.Size = new System.Drawing.Size(22, 13);
+            this.llblOK.TabIndex = 4;
+            this.llblOK.TabStop = true;
+            this.llblOK.Text = "OK";
+            this.llblOK.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnk_LinkClicked);
+            // 
+            // llblCancel
+            // 
+            this.llblCancel.AutoSize = true;
+            this.llblCancel.Location = new System.Drawing.Point(217, 75);
+            this.llblCancel.Name = "llblCancel";
+            this.llblCancel.Size = new System.Drawing.Size(40, 13);
+            this.llblCancel.TabIndex = 5;
+            this.llblCancel.TabStop = true;
+            this.llblCancel.Text = "Cancel";
+            this.llblCancel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnk_LinkClicked);
+            // 
+            // AlbumPasswordDialog
+            // 
+            this.AcceptButton = this.llblOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.llblCancel;
+            this.ClientSize = new System.Drawing.Size(269, 97);
+            this.ControlBox = false;
+            this.Controls.Add(this.llblCancel);
+            this.Controls.Add(this.llblOK);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtAlbum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "AlbumPasswordDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Please Enter Password";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAlbum;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.LinkLabel llblOK;
+        private System.Windows.Forms.LinkLabel llblCancel;
+    }
+}
